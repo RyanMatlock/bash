27136189ef23ed2d90662cfdb2f952ebbba7fefd
Ryan Matlock <ryan.matlock@gmail.com>
2014-12-17 14:21:33 -0800

    let's see how long these batteries last on the RPi

---
+++ scripts/uptime_til_shutdown.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# uptime_til_shutdown.sh
+
+# This script writes the output of the program uptime to a file, ~/uptime.log,
+# every minute It's useful for when you want to know how long a computer runs
+# until it's shutdown.
+
+# By the way, you may want to call it with a trailing ampersand (like
+# ./uptime_til_shutdown.sh &) so that you get your shell back.
+
+while true; do
+    sleep 1m # uptime only records to the minute anyway
+    echo $(uptime) > ~/uptime.log
+done
+
